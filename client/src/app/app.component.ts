e6baacea7508c0e2e8e8a183bbf367e93200d073
Aaron <aaron@Aarons-MacBook-Pro.local>
2020-12-30 18:03:13 +0800

    save

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +6,10 @@ import { IPagination } from './models/pagination';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'client';
-  products: IProduct[];
+  title = 'Skinet';
 
-  constructor(private http: HttpClient) {}
+  constructor() { }
 
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/products").subscribe(
-    (response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }
---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PaginationModule, CarouselModule} from 'ngx-bootstrap';
+import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
+import { PagerComponent } from './components/pager/pager.component';
+
+@NgModule({
+  declarations: [PagingHeaderComponent, PagerComponent],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
+  ],
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent,
+    PagerComponent,
+    CarouselModule
+  ]
+})
+export class SharedModule { }
